05b95177d4d5a59e20bb8a024d1d36a236320c5d
Marcus Breese <mbreese@stanford.edu>
2015-07-16 16:35:01 -0700

    added ctor to set channel and name for reader

--- src/java/io/compgen/common/StringLineReader.java
+++ src/java/io/compgen/common/StringLineReader.java
@@ -2,10 +2,14 @@ package io.compgen.common;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.FileChannel;
 
 public class StringLineReader extends AbstractLineReader<String> {
     public StringLineReader(InputStream is) {
         super(is);
+    }
+    public StringLineReader(InputStream is, FileChannel channel, String name) {
+        super(is, channel, name);
     }
 
     public StringLineReader(String filename) throws IOException {
