844fd9e3de397d7c0f2aeb9a57eb553fcab01f95
Marcus Breese <marcus@breese.com>
2025-12-11 01:00:38 -0500

    small refactor of fileutils

--- src/java/io/compgen/common/FileUtils.java
+++ src/java/io/compgen/common/FileUtils.java
@@ -1,9 +1,36 @@
 package io.compgen.common;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class FileUtils {
+	public FileUtils() {
+	}
+    public static OutputStream openOutputStream(String outputName) throws IOException {
+    	return openOutputStream(outputName, false);
+    }
 
+    public static OutputStream openOutputStream(String outputName, boolean forceCompress) throws IOException {
+    	if (forceCompress) {
+        	if (outputName == null || outputName.equals("-")) {
+                return new GZIPOutputStream(System.out);
+            } else {
+                return new GZIPOutputStream(new FileOutputStream(outputName));
+            }
+    	} else {
+	    	if (outputName == null || outputName.equals("-")) {
+	            return System.out;
+	        } else if (outputName.endsWith(".gz")) {
+	            return new GZIPOutputStream(new FileOutputStream(outputName));
+	        } else {
+	            return new BufferedOutputStream(new FileOutputStream(outputName));
+	        }
+    	}
+    }
 	/**
 	 * Replace ~/foo/bar filenames with $HOME/foo/bar
 	 * 
@@ -20,4 +47,5 @@ public class FileUtils {
 		}
 		return path;
 	}
+
 }
